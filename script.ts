8588c4e6ef833c5e7508c701ff061e1cdc20f08f
coshtya <kkletsyn@mail.ru>
2022-06-04 20:11:57 +0300

    Push unsaved .ts changes

--- script.ts
+++ script.ts
@@ -1,14 +1,18 @@
 const DEFAULT_STEP_NUMBER = 1;
 
-const test = () => {
+// set first step as active if no active found
+const getCurrentStep = (): HTMLElement | undefined => {
   const form = document.querySelector('[multistep-form]');
   if (!form) {
     return;
   }
   const steps = [...form.querySelectorAll<HTMLElement>('[step]')];
-  const index = steps.findIndex((step) => {
+  let currentStepIndex = steps.findIndex((step) => {
     return step.classList.contains('active');
   });
-  const currentStep = index >= 0 ? steps[index] : steps[DEFAULT_STEP_NUMBER];
-  console.log(currentStep); 
+  if (currentStepIndex < 0) {
+    currentStepIndex = 0;
+    steps[currentStepIndex].classList.add('active');
+  }
+  return steps[currentStepIndex];
 }
