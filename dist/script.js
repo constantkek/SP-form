962b659bdeb88f4e92ff0cb96d4478374fe676b8
coshtya <kkletsyn@mail.ru>
2022-06-05 01:04:52 +0300

    Reformat ts file to OOP style

--- dist/script.js
+++ dist/script.js
@@ -1,17 +1,42 @@
 "use strict";
-const DEFAULT_STEP_NUMBER = 1;
-const getCurrentStep = () => {
-    const form = document.querySelector('[multistep-form]');
-    if (!form) {
-        return;
+const DEFAULT_STEP_INDEX = 0;
+class Form {
+    constructor() {
+        this.steps = [...document.querySelectorAll('[step]')];
+        this.prevButtons = [...document.querySelectorAll('[btn-prev]')];
+        this.nextButtons = [...document.querySelectorAll('[btn-next]')];
+        this.addEventListeners();
+        this.currentStepIndex = this.getCurrentStepIndex();
     }
-    const steps = [...form.querySelectorAll('[step]')];
-    let currentStepIndex = steps.findIndex((step) => {
-        return step.classList.contains('active');
-    });
-    if (currentStepIndex < 0) {
-        currentStepIndex = 0;
-        steps[currentStepIndex].classList.add('active');
+    getCurrentStepIndex() {
+        if (!this.steps.length) {
+            throw new Error('Steps must be defined');
+        }
+        let currentStepIndex = this.steps.findIndex((step) => {
+            return step.classList.contains('active');
+        });
+        if (currentStepIndex < 0) {
+            currentStepIndex = DEFAULT_STEP_INDEX;
+            this.steps[currentStepIndex].classList.add('active');
+        }
+        return currentStepIndex;
     }
-    return steps[currentStepIndex];
-};
+    updateCurrentStep(index) {
+        this.steps[this.currentStepIndex].classList.remove('active');
+        this.currentStepIndex = index;
+        this.steps[this.currentStepIndex].classList.add('active');
+    }
+    addEventListeners() {
+        this.prevButtons.forEach((button, index) => {
+            button.addEventListener('click', () => {
+                this.updateCurrentStep(index);
+            });
+        });
+        this.nextButtons.forEach((button, index) => {
+            button.addEventListener('click', () => {
+                this.updateCurrentStep(index + 1);
+            });
+        });
+    }
+}
+const form = new Form();
--- script.ts
+++ script.ts
@@ -1,18 +1,51 @@
-const DEFAULT_STEP_NUMBER = 1;
+const DEFAULT_STEP_INDEX = 0;
 
-// set first step as active if no active found
-const getCurrentStep = (): HTMLElement | undefined => {
-  const form = document.querySelector('[multistep-form]');
-  if (!form) {
-    return;
+class Form {
+  private steps: HTMLElement[] = [...document.querySelectorAll<HTMLElement>('[step]')];
+  private prevButtons: HTMLElement[] = [...document.querySelectorAll<HTMLElement>('[btn-prev]')];
+  private nextButtons: HTMLElement[] = [...document.querySelectorAll<HTMLElement>('[btn-next]')];
+
+  private currentStepIndex: number;
+
+  public constructor() {
+    this.addEventListeners();
+    this.currentStepIndex = this.getCurrentStepIndex()
   }
-  const steps = [...form.querySelectorAll<HTMLElement>('[step]')];
-  let currentStepIndex = steps.findIndex((step) => {
-    return step.classList.contains('active');
-  });
-  if (currentStepIndex < 0) {
-    currentStepIndex = 0;
-    steps[currentStepIndex].classList.add('active');
+
+  // set first step as active if no active found
+  private getCurrentStepIndex(): number {
+    if (!this.steps.length) {
+      throw new Error('Steps must be defined');
+    }
+    let currentStepIndex = this.steps.findIndex((step) => {
+      return step.classList.contains('active');
+    });
+    if (currentStepIndex < 0) {
+      currentStepIndex = DEFAULT_STEP_INDEX;
+      this.steps[currentStepIndex].classList.add('active');
+    }
+    return currentStepIndex;
   }
-  return steps[currentStepIndex];
+
+  private updateCurrentStep(index: number): void {
+    this.steps[this.currentStepIndex].classList.remove('active');
+    this.currentStepIndex = index;
+    this.steps[this.currentStepIndex].classList.add('active');
+  }
+
+  private addEventListeners(): void {
+    this.prevButtons.forEach((button, index) => {
+      button.addEventListener('click', () => {
+        this.updateCurrentStep(index);
+      });
+    });
+    
+    this.nextButtons.forEach((button, index) => {
+      button.addEventListener('click', () => {
+        this.updateCurrentStep(index + 1);
+      });
+    });
+  }
 }
+
+const form = new Form();
